e4996b6749c5a004da99223b5567691e9d14286b
Patrick Hogan <phogan110@live.com>
2018-10-18 11:57:48 -0600

    added h1, with text

--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
-// new div
-document.addEventListener("DOMContentLoaded", function() {
-    var headerContainer = document.createElement('div');
-    // var headerText = document.createTextNode("the headerContainer is working"); this creates the variable to test headerContainer's functionality
+document.addEventListener("DOMContentLoaded", function () {
+    var div1 = document.createElement('div');
+    var div2 = document.createElement('div');
     
-    // this line assigns a classname of "header-container" to headerContainer
-    headerContainer.className = "header-container";
+    // var headerText = document.createTextNode("the headerContainer is working");// this creates the variable to test headerContainer's functionality
+
+    div1.className = "header-container";// this line assigns a classname of "header-container" to headerContainer
+
+    document.body.appendChild(div1);// this line pulls div1 into the body of the html
     
-    // this line pulls headerContainer into the body of the html
-    document.body.appendChild(headerContainer);
+    var headerContainer = document.getElementsByClassName("header-container");
+    // headerContainer[0].appendChild(headerText);// this puts text in headerContainer to ensure it is working
     
-    // document.body.appendChild(headerText); this puts text in headerContainer to ensure it is working
+    var h1 = document.createElement('h1');
+    var h1Text = document.createTextNode("This is an h1");
+
+    h1.appendChild(h1Text);//this adds the h1Text to h1
+    headerContainer[0].appendChild(h1);//this creates the h1 in the 'header-container' `div`
 
 
 });
